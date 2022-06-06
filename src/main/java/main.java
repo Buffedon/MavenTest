0b2cbadf683cd3974a6cf8a53c15ae6f6dc72452
Buffedon <1551969058@qq.com>
2022-06-06 11:50:00 +0800

    second commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
